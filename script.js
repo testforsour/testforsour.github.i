aedd5814ca066aa29deba2c2580630d463a4e14b
testforsour <70774647+testforsour@users.noreply.github.com>
2020-09-06 14:13:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+var sec = 0;
+function pad ( val ) { return val > 9 ? val : "0" + val; }
+setInterval( function(){
+    $("#seconds").html(pad(++sec%60));
+    $("#minutes").html(pad(parseInt(sec/60,10)));
+}, 1000);
